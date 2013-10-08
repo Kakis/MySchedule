8222827b9d467d964e2e5a217c993d3edaaacc9f
Jens <jens.hagfeldt@gmail.com>
2013-10-08 16:24:52 +0200

    adding project to git

---
+++ My ScheduleTests/My_ScheduleTests.m
@@ -0,0 +1,34 @@
+//
+//  My_ScheduleTests.m
+//  My ScheduleTests
+//
+//  Created by Jens Hagfeldt on 2013-10-04.
+//  Copyright (c) 2013 SEA - Smart Entertaining Apps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_ScheduleTests : XCTestCase
+
+@end
+
+@implementation My_ScheduleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
